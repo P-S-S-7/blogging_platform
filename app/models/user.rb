157278a729d31686a9a27db675d17c7f11394f59
P-S-S-7 <psinghshekhawat982@gmail.com>
2025-01-17 16:59:13 +0530

    Fix #3: screenshots deletion

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
 	has_many :blogs, dependent: :destroy
 	normalizes :email, with: ->(e) { e.strip.downcase }
 
-	validates :email, :presence => true, :uniqueness => true, :format => { :with => URI::MailTo::EMAIL_REGEXP, :message => "email format invalid" }
+	validates :email, :presence => true, 
+			:uniqueness => true, 
+			:format => { :with => URI::MailTo::EMAIL_REGEXP, :message => "email format invalid" }
 end
